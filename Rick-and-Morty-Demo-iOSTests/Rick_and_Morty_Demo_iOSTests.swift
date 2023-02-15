5b3a204011dfcb2f5112e989e0ec0a733cfbeca9
Palancica Pavel <ppalancica@Ds-MacBook-Pro.local>
2023-02-15 15:29:17 +0200

    first commit

---
+++ Rick-and-Morty-Demo-iOSTests/Rick_and_Morty_Demo_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rick_and_Morty_Demo_iOSTests.swift
+//  Rick-and-Morty-Demo-iOSTests
+//
+//  Created by Palancica Pavel on 15.02.2023.
+//
+
+import XCTest
+@testable import Rick_and_Morty_Demo_iOS
+
+class Rick_and_Morty_Demo_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
